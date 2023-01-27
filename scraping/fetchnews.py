5243d3525cc8f098d121e727b3dc16ebc24f90a8
Thomas Sirvent <sirventhomas@gmail.com>
2023-01-27 07:55:05 -0500

    Exemple data and script

---
+++ scraping/fetchnews.py
@@ -0,0 +1,28 @@
+import http.client
+import json
+from datetime import datetime
+from decouple import config
+import pandas as pd
+
+
+conn = http.client.HTTPSConnection("bing-news-search1.p.rapidapi.com")
+
+headers = {
+    'X-BingApis-SDK': "true",
+    'X-RapidAPI-Key': config('RAPIDAPIKEY'),
+    'X-RapidAPI-Host': "bing-news-search1.p.rapidapi.com"
+}
+
+conn.request("GET", "/news?textFormat=Raw&safeSearch=Moderate&category=Technology", headers=headers)
+
+
+res = conn.getresponse().read().decode("utf-8")
+jres = json.loads(res)
+df = pd.json_normalize(jres, record_path=['value'])
+
+# pd.set_option('display.max_colwidth', None)
+
+now = datetime.now()
+datestr = now.strftime("%d%b-%H%M%S")
+
+df.to_csv(f'scraped/{datestr}.csv')
