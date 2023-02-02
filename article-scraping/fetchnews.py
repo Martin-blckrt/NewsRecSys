2c52ed2238141c7d48a0dbc4c0cfd0e4dbf661d5
Thomas Sirvent <sirventhomas@gmail.com>
2023-02-02 11:26:15 -0500

    update git ignore

--- article-scraping/fetchnews.py
+++ article-scraping/fetchnews.py
@@ -18,11 +18,11 @@ conn.request("GET", "/news?textFormat=Raw&safeSearch=Moderate&category=Technolog
 
 res = conn.getresponse().read().decode("utf-8")
 jres = json.loads(res)
-df = pd.json_normalize(jres, record_path=['value'])
+df = pd.json_normalize(jres)
 
 # pd.set_option('display.max_colwidth', None)
 
 now = datetime.now()
 datestr = now.strftime("%d%b-%H%M%S")
 
-df.to_csv(f'scraped/{datestr}.csv')
+df.to_csv(f'scraped_news/{datestr}.csv')
