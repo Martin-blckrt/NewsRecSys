bbc45ab455e3c105440fe8a75f674750a4663d1d
Thomas Sirvent <sirventhomas@gmail.com>
2023-02-09 21:01:17 -0500

    newsapi usable and csv reader

---
+++ article-scraping/look_csv.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+def print_full(x):
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 2000)
+    pd.set_option('display.float_format', '{:20,.2f}'.format)
+    pd.set_option('display.max_colwidth', None)
+    print(x)
+    pd.reset_option('display.max_rows')
+    pd.reset_option('display.max_columns')
+    pd.reset_option('display.width')
+    pd.reset_option('display.float_format')
+    pd.reset_option('display.max_colwidth')
+
+
+df = pd.read_csv('scraped_news/09Feb-204819.csv', )
+
+print_full(df.head(5))
