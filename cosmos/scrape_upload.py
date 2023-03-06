dad2bf9756d10d873ae48699134a1d7fd3fc1531
Martin Blanckaert <57911418+Martin-blckrt@users.noreply.github.com>
2023-03-05 19:37:02 -0500

    scrape upload uploads to azure cosmos db

--- cosmos/scrape_upload.py
+++ cosmos/scrape_upload.py
@@ -1,13 +1,13 @@
 import azure.cosmos.cosmos_client as cosmos_client
-import csv
 import requests
 import datetime
 import pandas as pd
 import tweepy as tw
-# from decouple import config
-# from newsapi import NewsApiClient
+from newsapi import NewsApiClient
 import json
 import urllib.request
+import re
+import numpy as np
 
 
 def reddit():
@@ -72,14 +72,15 @@ def reddit():
                         if post['data'][feature] < CRITERIA[sub][feature]:
                             continue
 
-                obj = {'subreddit': post['data']['subreddit'],
-                       'link_flair_text': post['data']['link_flair_text'],
+                obj = {'source': post['data']['subreddit'],
+                       'tag': post['data']['link_flair_text'],
                        'title': post['data']['title'],
-                       'selftext': post['data']['selftext'],
+                       'description': post['data']['selftext'],
                        'author': post['data']['author'],
                        'url': post['data']['url'],
-                       'ups': post['data']['ups'],
-                       'downs': post['data']['downs']
+                       'likes': post['data']['ups'],
+                       'downs': post['data']['downs'],
+                       'time': datetime.datetime.fromtimestamp(int(post['data']['created_utc']))
                        }
                 results.append(obj)
     return results
@@ -127,10 +128,10 @@ def twitter():
                 "FrnkNlsn",
                 "SchmidhuberAI",
                 "CSProfKGD",
-                "ccanonne_",
-                "Michael_J_Black",
-                "heyBarsee",
-                "ptrblck_de",
+                # "ccanonne_",
+                # "Michael_J_Black",
+                # "heyBarsee",
+                # "ptrblck_de",
                 "mervenoyann"]
 
     KEYWORDS = ["Generative AI",
@@ -184,15 +185,18 @@ def twitter():
             author_metrics = client.get_user(id=tweet.author_id, user_fields=['public_metrics'])
 
             # Create a dictionary representing the tweet
-            obj = {'id': tweet.id,
-                   'author': tweet.author_id,
+            obj = {'tweet_id': tweet.id,
+                   'author': str(tweet.author_id),
                    'followers': author_metrics.data.public_metrics['followers_count'],
-                   'text': text,
+                   'title': text,
                    'likes': tweet.public_metrics['like_count'],
                    "quotes": tweet.public_metrics['quote_count'],
                    "retweets": tweet.public_metrics['retweet_count'],
                    "replies": tweet.public_metrics['reply_count'],
-                   "impressions": tweet.public_metrics['impression_count']
+                   "impressions": tweet.public_metrics['impression_count'],
+                   "url": "fakeTwitterURL",
+                   "time": datetime.datetime.now(),
+                   "source": "twitter"
                    }
             results.append(obj)  # Add the tweet to the results
 
@@ -200,7 +204,7 @@ def twitter():
 
 
 def newsapi():
-    newsapi = NewsApiClient(api_key=config('NEWSAPIKEY'))
+    newsapi = NewsApiClient(api_key="f54af54ed2164805824348bfaac1d534")
 
     # /v2/top-headlines
     top_tech = newsapi.get_top_headlines(category='technology', language='en', page_size=50)
@@ -213,7 +217,7 @@ def newsapi():
 
 
 def gnews():
-    apikey = config("GNEWSAPIKEY")
+    apikey = "1f956a6b862d056eda8a3b62f3fc4c9b"
     category = "technology"
     url = f"https://gnews.io/api/v4/top-headlines?topic={category}&token={apikey}&lang=en&country=us&max=10"
 
@@ -242,28 +246,48 @@ def init():
     return client
 
 
-def upsert(df, container):
-    # Write rows of a pandas DataFrame as items to the Database Container
-    for i in range(0, df.shape[0]):
-        # create a dictionary for the selected row
-        data_dict = dict(df.iloc[i, :])
-        # convert the dictionary to a json object.
-        data_dict = json.dumps(data_dict)
-        container.upsert_item(body=json.loads(data_dict))
-    print('Records inserted successfully.')
-
-
 def getdata():
     df_reddit = pd.DataFrame(reddit())
     df_twitter = pd.DataFrame(twitter())
     df_newsapi = pd.DataFrame(newsapi())
     df_gnews = pd.DataFrame(gnews())
 
-    df_twitter['author'] = df_twitter['author'].astype(str)
+    df_newsapi = df_newsapi.rename(columns={"source.name": "source",
+                                            "source.id": "source.url",
+                                            "urlToImage": "image",
+                                            "publishedAt": "time"})
 
-    merged_1 = pd.merge(df_reddit, df_twitter, on=['author'], how='outer')
-    merged_2 = pd.merge(merged_1, df_gnews, on=['title'], how='outer')
-    return pd.merge(merged_2, df_newsapi, on=['title', 'author'], how='outer')
+    df_gnews = df_gnews.rename(columns={"source.name": "source",
+                                        "publishedAt": "time"})
+
+    merged_social = pd.merge(df_reddit, df_twitter, on=['author', 'url', 'title', 'time', 'likes', 'source'],
+                             how='outer')
+    merged_news = pd.merge(df_newsapi, df_gnews,
+                           on=['title', 'url', 'time', 'source', 'description', 'content', 'source.url', 'image'],
+                           how='outer')
+
+    merged_news['time'] = merged_news['time'].apply(
+        lambda x: datetime.datetime.strptime(x.replace('T', ' ').replace('Z', ''), '%Y-%m-%d %H:%M:%S'))
+
+    df = pd.merge(merged_social, merged_news, on=['title', 'url', 'time', 'description', 'author', 'source'],
+                    how='outer')
+
+    df['time'] = df['time'].astype(str)
+    df = df.reset_index()
+    df = df.rename(columns={'index': 'id'})
+    df['id'] = df['id'].astype(str)
+    df = df.replace(np.nan, 0)
+
+    return df
+
+
+def upsert(df, container):
+    # Write rows of a pandas DataFrame as items to the Database Container
+    for i in range(0, df.shape[0]):
+        # create a dictionary for the selected row
+        data_dict = dict(df.iloc[i, :])
+        container.upsert_item(body=data_dict)
+    print('Records inserted successfully.')
 
 
 if __name__ == '__main__':
@@ -271,6 +295,6 @@ if __name__ == '__main__':
     database = client.get_database_client('newsData')
     container = database.get_container_client('newsContainer')
     df = getdata()
-    # upsert(df, container)
+    upsert(df, container)
 
     # upsert/create/replace/read/read_all/conflict
